0daf8d8b98a9b8157d2604b82026c604ad0c38c4
Robinlovelace <rob00x@gmail.com>
2015-08-17 18:30:00 +0100

    Mini updates

--- R/load-gs.R
+++ R/load-gs.R
@@ -1,16 +1,15 @@
 # aim: download data used in the challenge
 
-install.packages("googlesheets")
+# install.packages("googlesheets")
 
 library(googlesheets)
 library(magrittr)
 
+# setup
 gs_gap() %>%
   gs_copy(to = "Gapminder")
-## or, if you don't use pipes
-gs_copy(gs_gap(), to = "Gapminder")
-
-gs_ls()
+#
+# gs_ls()
 
 gs1 <- googlesheets::gs_url("https://docs.google.com/spreadsheets/d/1vwEFSpq-NraENZNxzmS1MMf4PyXsUz93p9pjCKe02do/edit#gid=0")
 df <- googlesheets::gs_read(gs1)
---
+++ R/test-plots.R
@@ -0,0 +1,5 @@
+# test plots
+
+source("R/load-gs.R")
+
+plot(df$lonWGS84, df$latWGS84)
