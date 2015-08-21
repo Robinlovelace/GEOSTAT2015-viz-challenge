987054c399a7bc25811eda0feb4ecaba3f46a9d9
Robinlovelace <rob00x@gmail.com>
2015-08-21 14:21:50 +0100

    Add my prediction script

--- R/load-gs.R
+++ R/load-gs.R
@@ -11,8 +11,13 @@ gs_gap() %>%
 #
 # gs_ls()
 
-gs1 <- googlesheets::gs_url("https://docs.google.com/spreadsheets/d/1vwEFSpq-NraENZNxzmS1MMf4PyXsUz93p9pjCKe02do/edit#gid=0")
+gs1 <- googlesheets::gs_url("https://docs.google.com/spreadsheets/d/1UnR-6bTRYz_QTZhpSl5_QgbCdrpMkuEui2YZgkCaFrg/edit#gid=1854940019")
 df <- googlesheets::gs_read(gs1)
 head(df)
 dir.create("data")
-write.csv(df, "data/df.csv")
+# write.csv(df, "data/df.csv")
+
+pred <- gs_read(ss = gs1, ws = 3)
+head(pred)
+
+
--- setup.R
+++ setup.R
@@ -5,3 +5,15 @@ library(lubridate)
 df$time <- mdy(df$date)
 cds <- as.matrix(cbind(df$lonWGS84, df$latWGS84))
 d <- SpatialPointsDataFrame(cds, data = df)
+
+source("R/load-gs.R")
+sel <- is.na(pred$lonWGS84)
+pred[sel,] # NAs
+pred <- pred[!sel,]
+pred$time <- mdy(pred$date)
+plot(pred$time)
+dpredm <- as.matrix(cbind(pred$lonWGS84, pred$latWGS84))
+
+pred <- as.data.frame(pred)
+dpred <- SpatialPointsDataFrame(dpredm, pred)
+
